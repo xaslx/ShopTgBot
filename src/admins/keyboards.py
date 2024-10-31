21582dac5d65cbe473ad370653a2e09fdcbc6816
Maksim <knyazev.maksim2852@gmail.com>
2024-10-31 21:26:23 +0400

    add keyboard

--- src/admins/keyboards.py
+++ src/admins/keyboards.py
@@ -1,7 +1,12 @@
 from typing import Any
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+from aiogram import Bot
+from config import env_config
 
 
+
+bot: Bot = Bot(token=env_config.TOKEN_BOT)
+ADMINS_ID: list[int] = env_config.ADMINS_ID
 
 
 
@@ -32,3 +37,16 @@ def confirm_delete_item(article: int) -> InlineKeyboardMarkup:
         ]
     )
     return inline_kb
+
+
+
+def admin_commands() -> ReplyKeyboardMarkup:
+    kb: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text='Сделать рассылку')],
+            [KeyboardButton(text='Добавить новый товар')],
+            [KeyboardButton(text='Отменить действие')]
+        ],
+        resize_keyboard=True
+    )
+    return kb
