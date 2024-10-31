3bbd8763e874bf4666006fdc278bf2b237007d0b
Maksim <knyazev.maksim2852@gmail.com>
2024-10-31 14:41:46 +0400

    del func

--- src/admins/handlers.py
+++ src/admins/handlers.py
@@ -11,6 +11,7 @@ from src.models.item import Item
 from src.repositories.item import ItemRepository
 from src.schemas.item import NewItemSchema
 from src.admins.keyboards import confirm_delete_item
+from src.admins.utils import update_item, edit_item
 
 
 admin_handler: Router = Router(name='Admin Router')
@@ -162,39 +163,6 @@ async def delete_item(callback: CallbackQuery):
     await callback.answer()
     await callback.message.delete()
     await callback.message.answer('Вы отменили удаление товара')
-
-
-
-async def edit_item(callback: CallbackQuery, state: FSMContext, item_type: str, state_type):
-    _, _, article = callback.data.split(':')
-    await state.update_data({'article': article})
-    await callback.answer()
-    await callback.message.delete()
-    await callback.message.answer(
-        text=f'Введите новое {item_type} для товара\n'
-             'Или <b>/acancel</b> чтобы отменить'
-    )
-    await state.set_state(state_type)
-
-
-async def update_item(message: Message, state: FSMContext, session: AsyncSession, item_type: str, **values):
-    data: dict = await state.get_data()
-    await state.clear()
-    res: Item | None = await ItemRepository.update(session=session, article=int(data['article']), **values)
-    if res:
-        await message.answer(f'Вы успешно поменяли {item_type} для товара\nАртикул: <b>{data["article"]}</b>\nТовар теперь выглядит так:\n')
-        await message.answer_photo(
-        photo=res.photo_id,
-        caption=
-        f'Артикул: <b>{res.article}</b>\n\n'
-        f'Название: <b>{res.title}</b>\n\n'
-        f'Описание: <b>{res.description}</b>\n\n'
-        f'Цена: <b>{res.price}</b>\n\n'
-        f'Количество: <b>{res.quantity}</b>',
-    )
-    else:
-        await message.answer(f'Не удалось изменить {item_type}')
-    
 
 
 
