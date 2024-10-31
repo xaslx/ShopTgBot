757411da6446743f45059f85dcaa1d5cdc2472b6
Maksim <knyazev.maksim2852@gmail.com>
2024-10-31 15:02:04 +0400

    add new field

--- src/models/item.py
+++ src/models/item.py
@@ -1,16 +1,16 @@
 from database import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, Sequence
+from sqlalchemy import Integer, Sequence, Identity, ARRAY
 
 
 
 class Item(Base):
     __tablename__ = 'items'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    article: Mapped[Integer] = mapped_column(Integer, Sequence(name='Article', start=100000, increment=1), unique=True)
+    article: Mapped[int] = mapped_column(Identity(start=100000, increment=1), primary_key=True)
     title: Mapped[str]
     description: Mapped[str]
     price: Mapped[float]
     quantity: Mapped[int] = mapped_column(default=0)
+    sizes: Mapped[list[int]] = mapped_column(ARRAY(Integer))
     photo_id: Mapped[str]
