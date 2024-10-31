a2179e822ed44ba72314ebbb24d99fec1806484c
Maksim <knyazev.maksim2852@gmail.com>
2024-10-31 15:22:35 +0400

    update text

--- src/utils.py
+++ src/utils.py
@@ -3,11 +3,13 @@ from src.models.item import Item
 
 
 def get_item_into(item: Item) -> str:
+    sizes: list[str] = ', '.join(str(i) for i in item.sizes)
     item_info: str =  (
         f'Артикул: <b>{item.article}</b>\n\n'
         f'Название: <b>{item.title}</b>\n\n'
         f'Описание: <b>{item.description}</b>\n\n'
         f'Цена: <b>{item.price}</b>\n\n'
-        f'Количество: <b>{item.quantity}</b>'
+        f'Количество: <b>{item.quantity}</b>\n\n'
+        f'Размеры: <b>{sizes}</b>'
     )
     return item_info
