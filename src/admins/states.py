7d5f632a34fb3edb6ae5e1c803c8b56b06d74f00
Maksim <knyazev.maksim2852@gmail.com>
2024-10-31 13:39:38 +0400

    add admins states

---
+++ src/admins/states.py
@@ -0,0 +1,9 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class NewItem(StatesGroup):
+    title: State = State()
+    description: State = State()
+    price: State = State()
+    quantity: State = State()
+    photo_id: State = State()
